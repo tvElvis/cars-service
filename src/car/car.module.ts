6c0dff13fec9fdf63357c4bb08503aee31cf9c9e
illia <illyakolesnikov101@gmail.com>
2020-03-19 11:00:22 +0200

    import typeorm module to car and manufacturer modules

--- src/car/car.module.ts
+++ src/car/car.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CarService } from './car.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Car } from './car.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Car])],
   providers: [CarService]
 })
 export class CarModule {}
--- src/manufacturer/manufacturer.module.ts
+++ src/manufacturer/manufacturer.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ManufacturerService } from './manufacturer.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Manufacturer } from './manufacturer.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Manufacturer])],
   providers: [ManufacturerService]
 })
 export class ManufacturerModule {}
